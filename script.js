6f61f2ec7d325c404f287e1d7ac89f49a97ad59a
Adio Damilare Caleb <adiodamilare44@gmail.com>
2022-04-03 15:28:20 +1300

    I added background image and some styling to the alarm system

--- script.js
+++ script.js
@@ -9,16 +9,20 @@
             let hour = hr.value; 
             let mins = min.value;
             let seconds = sec.value;
+            var great = textarea.innerText
             t = new Date();
-            if(hour==t.getHours() && mins == t.getMinutes() && seconds == t.getSeconds()){
-                d.play();
-                alert("wakeup");
-                document.getElementById("alert").innerHTML=(`<div class="alert alert-danger alert-dismissible" role="alert">
-                This is a danger dismissible alert — check it out!
-                <button type="button" id="clear" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-              </div>`)
+
+           
+
+                if(hour==t.getHours() && mins== t.getMinutes() && seconds == t.getSeconds()){
+                    d.play();
+                    alert("wakeup");
+                    document.getElementById("alert").innerHTML=(`<div class="alert alert-danger alert-dismissible" role="alert">
+                   ${great}
+                    <button type="button" onclick="clear()" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                  </div>`)
                 };
-            setTimeout("setAlarm()",5000);
+                setTimeout(`setAlarm()`,5000);
            
         }
 
@@ -41,6 +45,5 @@
            
         }
 
-        document.getAnimations("clear").addEventListener("click",function(){
-            document.getElementById("alert").innerHTML=""
-        })
+       
+
