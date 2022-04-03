41264fc52a81e6b6ce1c42482adb090878dedbf9
Adio Damilare Caleb <adiodamilare44@gmail.com>
2022-04-04 06:22:39 +1300

    I added audio to the alarm system

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
  
         d= new Audio();
         d.src = "owo.mp3"
+        var cock = new Audio("Alaram.mp3")
         var h;
         var m;
         var s;
@@ -15,8 +16,8 @@
            
 
                 if(hour==t.getHours() && mins== t.getMinutes() && seconds == t.getSeconds()){
+                    cock.play();
                     d.play();
-                    alert("wakeup");
                     document.getElementById("alert").innerHTML=(`<div class="alert alert-danger alert-dismissible" role="alert">
                    ${great}
                     <button type="button" onclick="clear()" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
@@ -27,7 +28,6 @@
         }
 
         function stop(){    
-            d = new Date;
             a=prompt(`What is the answer of ${100} X ${49}/ ${2.5}`)
             if(a==1960){
                 var e = confirm("Are you sure you want to stop the alarm")?true:false;
